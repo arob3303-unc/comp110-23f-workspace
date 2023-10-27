7e32baac05fe6a6d5f6433c1f5b1e4c64cfe358a
arob3303-unc <arob3303@unc.edu>
2023-10-27 00:23:58 -0400

    ex06 done oct 27

--- exercises/ex06/dictionary.py
+++ exercises/ex06/dictionary.py
@@ -4,7 +4,7 @@ __author__ = "730717463"
 
 
 def invert(original_dict: dict[str, str]) -> dict[str, str]:
-    """keys, values for original dictionary swap and become a new dictionary."""
+    """The keys, values for original dictionary swap and become a new dictionary."""
     # empty dictionary
     new_dict: dict[str, str] = {}
 
@@ -14,25 +14,42 @@ def invert(original_dict: dict[str, str]) -> dict[str, str]:
         
     # raises key error if length of dict is shortened due to same key value!
     if len(original_dict) != len(new_dict):
-        raise KeyError("Not allowed to have same key value.")
-       
+        raise KeyError("Not allowed to have same key value.") 
     return new_dict
 
 
 def favorite_color(color_dict: dict[str, str]) -> str:
     """Returns the color that appears the most."""
-    fav_color: str = ""  # Holds the favorite color
-    color_counter: int = 0
+    # favorite color
+    fav_color: str = ""
+    # empty dictionary
+    new_dict: dict[str, int] = {}
+    # empty list
+    new_list: list[str] = []
 
+    # creates a list of the colors
     for key in color_dict:
-        if color_dict[key] in color_dict:
+        new_list.append(color_dict[key])
 
-            
+    # for loop to go through the list and assign it to the dict along with num count
+    for i in new_list:
+        # goes through list and assigns +1 as value for key if key is already in dict
+        if i in new_dict:
+            new_dict[i] += 1
+        # if key is not in dict, it assigns the number 1 to the value
+        else:
+            new_dict[i] = 1
+    
+    # holds max num to find favorite color
+    max_num: int = 0
 
-
-
-        #print(color_dict)
-print(favorite_color({"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}))
+    # for loop to find favorite color
+    for key in new_dict:
+        # if the max_num is less than the current value, it assigns it as the favorite color
+        if new_dict[key] > max_num:
+            fav_color = key
+            max_num = new_dict[key]
+    return fav_color
 
 
 def count(list_1: list[str]) -> dict[str, int]:
@@ -46,11 +63,40 @@ def count(list_1: list[str]) -> dict[str, int]:
             new_dict[i] = 1
     return new_dict
 
-#print(count(["a", "a", "a", "b", "c", "c"]))
-
 
 def alphabetizer(list1: list[str]) -> dict[str, list[str]]:
     """The words will be assigned to their starting letter (key)."""
+    # empty dictionary
+    new_dict: dict[str, list[str]] = {}
 
+    # goes through list to create keys for new dictionary
     for i in list1:
-        while 
+        new_dict[i[0].lower()] = []
+    
+    # for loop to go through keys and assign list values
+    for key in new_dict:
+        for i in list1:
+            if key == i[0].lower():  # if key equals the starting letter, assigns the value to key
+                new_dict[key].append(i)  # appends it since it is a list
+    return new_dict
+
+def update_attendance(attendance_log: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """Updates the attendance log (dictionary)."""
+    # updated attendance log
+    updated_log: dict[str, list[str]] = attendance_log
+
+    # empty string to hold the days (keys)
+    day_list: list[str] = []
+    # for loop to make list of days (keys)
+    for key in attendance_log:
+        day_list.append(key)
+    # if the day is not in the dictionary, adds the day and student and returns updated dictionary
+    if day not in day_list:
+        updated_log[day] = [student]  # assigns day and student as a new key, value
+        return updated_log  # returns it to prevent KeyError later on in function
+    # for loop to go through dictionary (only if it is a day that is already in dictionary)
+    for key in updated_log:
+        if key == day:  # if the day equals the day being recorded, student is added to the day
+            updated_log[day].append(student)
+    return updated_log  # returns the updated dictionary (log)
+ 
