f47637dd9878181fc5c23533cc761c02a0eb67ae
arob3303-unc <arob3303@unc.edu>
2023-11-16 00:13:57 -0500

    ex09

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -4,10 +4,28 @@ from __future__ import annotations
 
 from typing import Union
 
-__author__ = "YOUR PID HERE"
+__author__ = "730717463"
 
 
 class Simpy:
+    """Simpy Class."""
+    # Attributes
     values: list[float]
 
     # TODO: Your constructor and methods will go here.
+
+    def __init__(self, list_values: list[float]):
+        """Returns float values."""
+        self.values = list_values
+    
+
+    def __str__(self) -> str:
+        """Returns a readable str."""
+        msg: str = (f"Simply({self.values})")
+        return msg
+   
+    
+    def fill(self, num: float, amount: int) -> None:
+        """Makes a list with float numbers."""
+        
+        
