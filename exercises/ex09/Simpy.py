986588a38131ca3f40696c31792096158f0303a3
arob3303-unc <arob3303@unc.edu>
2023-11-23 20:23:25 -0500

    ex09

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -55,4 +55,45 @@ class Simpy:
         if self.values and self.values[-1] >= stop:
             self.values.pop()
 
+    def sum(self) -> float:
+        """Sums all values in self.values list."""
+        return sum(self.values)
+
+    def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Adds a number to each value in the Simpy list."""
+        new_list: list[float] = list()
+        i: int = 0
+        if type(rhs) is float:
+            for i in self.values:
+                new_list.append(rhs + i)
+        else:
+            assert len(self.values) == len(rhs.values)
+            for x in self.values:
+                new_list.append(x + rhs.values[i])
+                i += 1
+
+        return new_list
+
+    def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Exponentiation the value of lists."""
+        new_list: list[float] = list()
+        i: int = 0
+        if type(rhs) is float:
+            for i in self.values:
+                new_list.append(i ** rhs)
+        else:
+            assert len(self.values) == len(rhs.values)
+            for x in self.values:
+                new_list.append(x ** rhs.values[i])
+                i += 1
+
+        return new_list
     
+    
+
+
+a = Simpy([1.0, 1.0, 1.0])
+#print(a + 10.0)
+b = Simpy([2.0, 3.0, 4.0])
+c = a + b
+print(c)
